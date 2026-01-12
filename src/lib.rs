93471a5a80b6a66e32e9d016bb327d91706d414c
Taylor Plewe <tplewe@outlook.com>
2026-01-12 16:52:11 -0700

    get extension working with HTML and Vue files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
+use std::{env, fs};
+
 use zed_extension_api as zed;
 
 struct AxeAccessibilityLinter {}
+
+fn get_language_server_binary_path(worktree: &zed::Worktree) -> String {
+    if let Ok(path) = env::var("AXE_LS_PATH") {
+        if fs::metadata(&path).map_or(false, |metadata| metadata.is_file()) {
+            return path;
+        }
+    }
+    if let Some(path) = worktree.which("axe-ls") {
+        return path;
+    }
+    return "axe-ls".to_string();
+}
 
 impl zed::Extension for AxeAccessibilityLinter {
     fn new() -> Self {
@@ -10,12 +24,12 @@ impl zed::Extension for AxeAccessibilityLinter {
     fn language_server_command(
         &mut self,
         _language_server_id: &zed::LanguageServerId,
-        _worktree: &zed::Worktree,
-    ) -> Result<zed::Command> {
+        worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
         Ok(zed::Command {
-            command: "axe-ls".to_string(),
+            command: get_language_server_binary_path(worktree),
             args: vec!["--stdio".to_string()],
-            env: vec![],
+            env: Default::default(),
         })
     }
 }
